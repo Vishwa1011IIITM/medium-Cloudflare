d6944b66b4662f9d9d64ca9e34e1ede7a2fe1254
Vishwa1011 <imt_2022119@iiitm.ac.in>
2025-01-20 14:53:53 +0530

    Added User and Blogs Details

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 import { Hono } from 'hono'
+import { userRouter } from './routes/user';
+import { bookRouter } from './routes/blog';
 
-const app = new Hono()
+export const app = new Hono<{
+  Bindings: {
+      DATABASE_URL: string;
+      JWT_SECRET: string;
+  }
+}>();
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
-})
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
-})
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route('/api/v1/user', userRouter)
+app.route('/api/v1/book', bookRouter)
 
 export default app
